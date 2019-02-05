f55a1ee87163b1427a8aeb640d41fda5be5edc16
Gordon Young <gyoung@lpt-C02TP316HF1R.corp.verygood.systems>
2019-02-05 13:24:09 -0700

    add rollbar support

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 import sys
 import os
+import rollbar
 from flask import Flask, request, abort
 
 app = Flask(__name__)
 
+rollbar.init(os.environ.get('ROLLBAR_ACCESS_KEY'))
+
+
+def rollbar_handler(record):
+    message = 'title: Meraki Alert\n environment: external'.format(**record)
+    rollbar.report_message(message, level='error')
+
 
 @app.route('/', methods=['POST'])
-def webhook():
+def webhook() -> object:
     print("{\"action\":\"called webhook()\"}")
     sys.stdout.flush()
     if request.method == 'POST':
         if request.json['organizationId'] == os.environ['ORGANIZATION_ID']:
-            print(str(request.json).replace("u'", "\"").replace("'", "\""))
+            msg = str(request.json).replace("u'", "\"").replace("'", "\"")
+            print(msg)
+            rollbar_handler(request.json)
             print("{\"success\":\"200\"}")
             return "{\"success\":\"200\"}", 200
         else:
